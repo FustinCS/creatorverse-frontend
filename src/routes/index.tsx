e104b72ad49c9f504fd657cd53a57d26667caafa
Jose <josegonz115@gmail.com>
2024-11-09 23:46:37 -0800

    added rpc for user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,42 @@
 import { useEffect } from 'react'
 import { useUser } from '@clerk/clerk-react'
 import LandingPage from '../components/landing-page'
+import supabase from '../supabase/client'
+
+
 
 export default function IndexPage() {
-    const { isSignedIn, user} = useUser()
+    const { isSignedIn, user} = useUser();
+    // const [loading, setLoading] = useState(true)
+
+
+
+    useEffect(() => {
+        console.log('is signed in?', isSignedIn);
+        if (isSignedIn && user) {
+          const createOrUpdateUser = async () => {
+            const clerk_id = user.id;
+            const username = user.username;
+            if(!username){
+                console.error('no username');
+                return;
+            }
+            // const email = user.emailAddress;
+            const { data, error } = await supabase
+              .rpc('create_user_from_clerk', {
+                clerk_id: clerk_id,
+                username: username,
+              });
+    
+            if (error) {
+              console.error('Error creating/updating user:', error);
+            } else {
+              console.log('User created/updated successfully:', data);
+            }
+          };
+          createOrUpdateUser();
+        }
+      }, [isSignedIn, user]);
 
     if (!isSignedIn) {
         return (
