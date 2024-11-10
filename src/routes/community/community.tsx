698a3d6399a57aa72202045591821a88f7ae4c01
Nathan Chow <nathanchowcoding@gmail.com>
2024-11-09 20:17:03 -0800

    ADD: routes and layouts

---
+++ src/routes/community/community.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+export default function CommunityPage() {
+  return (
+    <>
+      <h1>Dashboard page</h1>
+      <p>This is a protected page.</p>
+
+      <ul>
+        <li>
+          <Link to="/community/"></Link>
+        </li>
+        <li>
+          <Link to="/">Return to index</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
