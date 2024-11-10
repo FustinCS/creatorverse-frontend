c3b1d99fa2f9b78880b0ec77b88df79167170115
Jose <josegonz115@gmail.com>
2024-11-10 04:03:26 -0800

    updated api

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,23 +1,64 @@
+import { useState } from "react";
+import { Plus, X } from "lucide-react";
 
 type TagType = {
     index: string;
     name: string;
     added: boolean;
     custom: boolean;
-    addTagHandler: (tagName: string) => void; 
-    removeTagHandler: (tagName: string) => void; 
+    addTagHandler: (tagName: string) => void;
+    removeTagHandler: (tagName: string) => void;
 };
 
-const Tag = ({index, name, added, custom, addTagHandler, removeTagHandler}: TagType) => {
-  return (
-    <span 
-        key={index} 
-        className="bg-muted text-muted-foreground text-sm py-1 px-2 rounded-full text-center"
-    >
-        {name}
+const Tag = ({
+    index,
+    name,
+    added,
+    custom,
+    addTagHandler,
+    removeTagHandler,
+}: TagType) => {
+    const [editName, setEditName] = useState(name);
 
-  </span>
-  )
-}
+    const handleButton = () => {
+        if (!added) {
+            if(editName === ''){
+                return;
+            }
+            addTagHandler(editName);
+            if(custom){
+                setEditName('');
+            }
+        } else {
+            removeTagHandler(name);
+        }
+    };
 
-export default Tag
+    return (
+        <span
+            key={index}
+            className="bg-slate-300 text-muted-foreground text-sm py-1 px-2 rounded-full text-center flex flex-row justify-between"
+        >
+            {custom ? (
+                <input
+                    type="text"
+                    value={editName}
+                    onChange={(e) => setEditName(e.target.value)}
+                    className=" font-bold text-center border-none outline-none bg-transparent text-black"
+                    placeholder="tag: "
+                />
+            ) : (
+                <p className="text-black font-bold text-center m-auto border-none outline-none bg-transparent">{name}</p>
+            )}
+            <button 
+                onClick={handleButton}
+                aria-label={!added ? "Add tag" : "Remove tag"}
+                // className="ml-2"
+            >
+            {!added ? <Plus /> : <X />}
+            </button>
+        </span>
+    );
+};
+
+export default Tag;
--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -3,5 +3,11 @@ import { Database } from "./database.types";
 
 export const URL = import.meta.env.VITE_SUPABASE_URL;
 export const API_KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+export const BUCKET_KEY = 'https://cobnjvyhgqvaownxctzp.supabase.co/storage/v1/object/public/';
+export const CREATOR_IMAGES = 'creator-images/';
+export const TEMP_IMAGES = 'temp-images/';
+
+
 const supabase = createClient<Database>(URL, API_KEY);
 export default supabase;
